d1b703450beb0431a4955e64c87855389f92f473
Roderick Randolph <me@roderickrandolph.com>
2014-06-19 00:30:11 -0400

    init

---
+++ run.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    path = require('path'),
+    person = require('./routes/person');
+    
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/people', person.findAll);
+app.get('/people/:id', person.findById);
+app.post('/people', person.addPerson);
+
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
