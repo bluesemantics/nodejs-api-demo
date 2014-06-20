0bc395e214e01b0f024fe570fbba0bf746030f7a
Roderick Randolph <me@roderickrandolph.com>
2014-06-19 21:11:17 -0400

    added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['nodejs-api-demo'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '84f3fdec90aec2f3d4cbe7dff5e9a4f00652acfb',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
