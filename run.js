6e1d6d76038b825d1a3d79235cf21b4c9dba8f58
Roderick Randolph <me@roderickrandolph.com>
2014-06-22 12:13:45 -0400

    debugging

--- run.js
+++ run.js
@@ -4,9 +4,27 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     http = require('http'),
     path = require('path'),
+    fs = require('fs'),
     person = require('./routes/person');
 
 var app = express();
+
+function getFiles(dir,files_){
+    files_ = files_ || [];
+    if (typeof files_ === 'undefined') files_=[];
+    var files = fs.readdirSync(dir);
+    for(var i in files){
+        if (!files.hasOwnProperty(i)) continue;
+        var name = dir+'/'+files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name,files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+console.log(getFiles('/tmp'))
 
 //app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
@@ -17,6 +35,6 @@ app.get('/people/:id', person.findById);
 app.post('/people', person.addPerson);
 
 
-http.createServer(app).listen('/tmp/nginx.socket', function () {
+//http.createServer(app).listen('/tmp/nginx.socket', function () {
     //console.log("Express server listening on port " + app.get('port'));
-});
+//});
