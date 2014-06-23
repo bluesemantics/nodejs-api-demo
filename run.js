413e5bae885e50a3d101f6da1274c6427c3a21c3
Roderick Randolph <me@roderickrandolph.com>
2014-06-23 08:40:11 -0400

    simplified code

--- run.js
+++ run.js
@@ -5,7 +5,54 @@ var express = require('express'),
     http = require('http'),
     path = require('path'),
     fs = require('fs'),
-    person = require('./routes/person');
+    mongo = require('mongodb'),
+    mongouri = require('mongo-uri');
+
+var Server = mongo.Server,
+    MongoClient = mongo.MongoClient,
+    BSON = mongo.BSONPure;
+
+var db;
+
+var addPerson = function(req, res) {
+    var person = req.body;
+    console.log('Adding person: ' + JSON.stringify(person));
+    //MongoClient.connect(process.env.MONGOLAB_URI, {native_parser:true}, function(err, db) {
+        db.collection('people', function(err, collection) {
+            collection.insert(person, {safe:true}, function(err, result) {
+                if (err) {
+                    res.send({'error':'An error has occurred'});
+                } else {
+                    console.log('Success: ' + JSON.stringify(result[0]));
+                    res.send(result[0]);
+                }
+            });
+        });
+    //});
+};
+
+var findAll = function(req, res) {
+    //res.send({});
+    //MongoClient.connect(process.env.MONGOLAB_URI, {native_parser:true}, function(err, db) {
+        db.collection('people', function(err, collection) {
+            collection.find().toArray(function(err, items) {
+                res.send(items);
+            });
+        });
+    //});
+};
+
+var findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving person: ' + id);
+    //MongoClient.connect(process.env.MONGOLAB_URI, {native_parser:true}, function(err, db) {
+        db.collection('people', function(err, collection) {
+            collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+                res.send(item);
+            });
+        });
+    //});
+};
 
 var app = express();
 
@@ -13,12 +60,17 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/people', person.findAll);
-app.get('/people/:id', person.findById);
-app.post('/people', person.addPerson);
+app.get('/people', findAll);
+app.get('/people/:id', findById);
+app.post('/people', addPerson);
 
 
-http.createServer(app).listen('/tmp/nginx.socket', function () {
-    //console.log("Express server listening on port " + app.get('port'));
-    fs.openSync('/tmp/app-initialized', 'w');
+MongoClient.connect(process.env.MONGOLAB_URI, {native_parser:true}, function(err, database) {
+    if (err) throw err;
+    db = database;
+
+    http.createServer(app).listen('/tmp/nginx.socket', function () {
+        //console.log("Express server listening on port " + app.get('port'));
+        fs.openSync('/tmp/app-initialized', 'w');
+    });
 });
